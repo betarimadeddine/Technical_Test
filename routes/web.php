dee4f83fda1e1493ff60db3a331622e43a2cf676
betar imad eddine <betarimadeddine@gmail.com>
2023-05-12 13:39:14 +0100

    update default route

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('categories.index');;
 })->name('home');
 
 Route::resource('categories', CategoryController::class);
